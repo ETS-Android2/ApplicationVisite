e8a80b68c0fd4ffa477580879ee3a8c5a885701b
thomas-frias <77983279+thomas-frias@users.noreply.github.com>
2021-11-02 16:59:41 +0100

    Ajout page de chargement (lors du lancement de l'application)

---
+++ app/src/main/java/com/example/applicationvisite/SplashScreenActivity.java
@@ -0,0 +1,31 @@
+package com.example.applicationvisite;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class SplashScreenActivity extends AppCompatActivity {
+
+    private final long BOOT_DELAY = 4000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        //redirige vers la page principale après 4 secondes
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+
+        //code éxécuté après les 4 secondes
+        Runnable run = new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(getApplicationContext(),MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        };
+        //Attente des 4 secondes avant de run
+        new Handler().postDelayed(run,BOOT_DELAY);
+    }
+}
