abd61b96b6e637cd84aa4952083b6040e9e55a6b
thomas-frias <77983279+thomas-frias@users.noreply.github.com>
2021-11-02 17:27:16 +0100

    changement couleurs par defaut

--- app/src/main/java/com/example/applicationvisite/MainActivity.java
+++ app/src/main/java/com/example/applicationvisite/MainActivity.java
@@ -39,7 +39,7 @@ public class MainActivity extends AppCompatActivity {
         binding.fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                Snackbar.make(view, "Il faudra créer une nouvelle vue avec le scanneur", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
             }
         });
