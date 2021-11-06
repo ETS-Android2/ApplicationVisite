d04a21bc401047d4b677fc8d04e8eade37b5726f
thomas-frias <thomas.frias@etu.umontpellier.fr>
2021-11-06 19:42:08 +0100

    correction et suppression du boutton précédent sur l'interface

--- app/src/main/java/com/example/applicationvisite/SecondFragment.java
+++ app/src/main/java/com/example/applicationvisite/SecondFragment.java
@@ -23,19 +23,6 @@ public class SecondFragment extends Fragment {
 
         binding = FragmentSecondBinding.inflate(inflater, container, false);
         return binding.getRoot();
-
-    }
-
-    public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
-        super.onViewCreated(view, savedInstanceState);
-
-        binding.buttonSecond.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                NavHostFragment.findNavController(SecondFragment.this)
-                        .navigate(R.id.action_SecondFragment_to_FirstFragment);
-            }
-        });
     }
 
     @Override
