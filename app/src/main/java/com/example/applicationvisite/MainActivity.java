28a1bf41c6b24c993cf06de9a0d15a9dd2c239da
thomas-frias <thomas.frias@etu.umontpellier.fr>
2021-11-08 21:36:47 +0100

    Qr scanner fonctionne (avec autorisation manuelle caméra)

--- app/src/main/java/com/example/applicationvisite/MainActivity.java
+++ app/src/main/java/com/example/applicationvisite/MainActivity.java
@@ -1,15 +1,35 @@
 package com.example.applicationvisite;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
 public class MainActivity extends AppCompatActivity {
+
+    private FloatingActionButton buttonForScanQR;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        buttonForScanQR = (FloatingActionButton) findViewById(R.id.qrButton);
+
+        buttonForScanQR.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+
+                Intent qrActivity = new Intent(getApplicationContext(),QrActivity.class);
+                startActivity(qrActivity);
+                finish();
+            }
+        });
+
     }
 
 }
---
+++ app/src/main/java/com/example/applicationvisite/QrActivity.java
@@ -0,0 +1,62 @@
+package com.example.applicationvisite;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import com.budiyev.android.codescanner.CodeScanner;
+import com.budiyev.android.codescanner.CodeScannerView;
+import com.budiyev.android.codescanner.DecodeCallback;
+import com.google.zxing.Result;
+
+public class QrActivity extends AppCompatActivity {
+    private CodeScanner mCodeScanner;
+    String resScan = null;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_qr);
+
+        scanQR();
+
+        System.out.println("Résultat du scan : " + resScan);
+    }
+
+    private void scanQR(){
+        CodeScannerView scannerView = findViewById(R.id.scanner_view);
+        mCodeScanner = new CodeScanner(this, scannerView);
+        mCodeScanner.startPreview();
+
+        mCodeScanner.setDecodeCallback(new DecodeCallback() {
+            @Override
+            public void onDecoded(@NonNull final Result result) {
+                //resultat !!!
+                resScan = result.getText();
+                finish();
+            }
+        });
+        scannerView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mCodeScanner.startPreview();
+            }
+        });
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        mCodeScanner.startPreview();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        mCodeScanner.releaseResources();
+    }
+
+}
