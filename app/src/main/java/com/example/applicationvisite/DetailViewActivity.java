2d26a2ff8591662ea261536ac378e2bfc843b2cc
thomas-frias <77983279+thomas-frias@users.noreply.github.com>
2021-11-13 16:22:16 +0100

    ajout du bouton fermer

--- app/src/main/java/com/example/applicationvisite/DetailViewActivity.java
+++ app/src/main/java/com/example/applicationvisite/DetailViewActivity.java
@@ -3,23 +3,36 @@ package com.example.applicationvisite;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.FragmentTransaction;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
 import com.example.applicationvisite.fragments.BatimentFragment;
 
 public class DetailViewActivity extends AppCompatActivity {
+
+    ImageButton buttonclose;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail_view);
 
-        //injecter le fragment batiment dans notre boite fragment container
+        //Injecter le fragment
+        FragmentTransaction t = getSupportFragmentManager().beginTransaction();
+        t.replace(R.id.fragment_container_detailview, new BatimentFragment());
+        t.addToBackStack(null);
+        t.commit();
 
-    FragmentTransaction t = getSupportFragmentManager().beginTransaction();
-    t.replace(R.id.fragment_container_detailview, new BatimentFragment());
-    t.addToBackStack(null);
-    t.commit();
+        buttonclose = (ImageButton) findViewById(R.id.button_close);
+        buttonclose.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent mainactivity = new Intent(getApplicationContext(),MainActivity.class);
+                startActivity(mainactivity);
+            }
+        });
 
     }
 }
