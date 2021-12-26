0a38e0d88bd35a9ee222f82d3cf22e64f9bdc9f3
thomas-frias <77983279+thomas-frias@users.noreply.github.com>
2021-12-26 15:35:11 +0100

    amélioration graphiques statistiques visite

--- app/src/main/java/com/example/applicationvisite/StatistiqueFragment.java
+++ app/src/main/java/com/example/applicationvisite/StatistiqueFragment.java
@@ -1,9 +1,13 @@
 package com.example.applicationvisite;
 
-import android.annotation.SuppressLint;
 import android.os.Build;
 import android.os.Bundle;
-import android.os.Debug;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.annotation.RequiresApi;
+import androidx.fragment.app.Fragment;
+
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -11,23 +15,19 @@ import android.view.ViewGroup;
 import android.widget.ProgressBar;
 import android.widget.TextView;
 
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
-import androidx.annotation.RequiresApi;
-import androidx.fragment.app.Fragment;
-
 import com.example.applicationvisite.logique.Batiment;
 
 import java.util.ArrayList;
 
-public class FooterFragment extends Fragment {
+public class StatistiqueFragment extends Fragment {
+
     private ArrayList<Batiment> list_bat_visited;
 
     @RequiresApi(api = Build.VERSION_CODES.O)
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        View rootView = inflater.inflate(R.layout.fragment_footer,container,false);
+        View rootView = inflater.inflate(R.layout.fragment_statistique,container,false);
 
         try {
 
@@ -38,20 +38,14 @@ public class FooterFragment extends Fragment {
                     cpt++;
                 }
             }
-            TextView bat_visited = (TextView) rootView.findViewById(R.id.nb_bat_visited);
-            String ressource = "Batiments visités " + cpt + "/" + list_bat_visited.size();
-            bat_visited.setText(ressource);
 
-            String tmp ="";
-            TextView batimentsRestants = (TextView) rootView.findViewById(R.id.text_bat_restant);
-            for (Batiment b:list_bat_visited) {
-                if (!b.isVisited()){
-                    tmp = tmp + b.getBat_nom() + "\n";
-                }
-            }
-            batimentsRestants.setText(tmp);
+            TextView pourcentage = (TextView) rootView.findViewById(R.id.text_bat_pourcentage_Stat);
+            int taux_progress = (int)((cpt*100)/list_bat_visited.size());
+            pourcentage.setText(taux_progress + " %");
 
-
+            ProgressBar progessbar = (ProgressBar) rootView.findViewById(R.id.progressBarStat);
+            //int ratio = (progessbar.getMin() + progessbar.getMax())/list_bat_visited.size() + cpt;
+            progessbar.setProgress(taux_progress);
         }
         catch (Exception e){
             Log.d("DEBUG", "Impossible de charger les éléments ! ils ne sont pas dans le layout actuel" );
